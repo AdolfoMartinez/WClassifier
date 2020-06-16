cca9e4fd9341b8fc05c72956f3e88851cad6ea73
Adolfo Martínez <adolfo.martinez.garza@gmail.com>
2020-06-16 11:45:03 -0500

    Linux support added.

--- Classifier.py
+++ Classifier.py
@@ -3,14 +3,26 @@ import time, os, shutil, sys
 from PIL import Image
 
 path = os.getcwd()
-new_folder_name = "Accepted"
+accepted_folder = "Accepted"
+rejected_folder = "Rejected"
+
+print(path)
 
 def extractor():
     for images in os.listdir():
-        full_image_path = path + str("\\") + str(images)
-
-        if not os.path.exists(new_folder_name):
-            os.makedirs(new_folder_name)
+        
+        if os.name == "posix":
+            full_image_path = path + "/" + str(images)
+        elif os.name == "nt":
+            full_image_path = path + str("\\") + str(images)
+        else:
+            print("Unknow OS")
+        
+        if not os.path.exists(accepted_folder):
+            os.makedirs(accepted_folder)
+        
+        if not os.path.exists(rejected_folder):
+            os.makedirs(rejected_folder)
 
         if images.endswith(".py"):
             continue
@@ -21,10 +33,16 @@ def extractor():
         if img.size[0] > img.size[1]:
             print("{} <- Rejected.".format(images))
             img.close()
+            shutil.move(full_image_path, rejected_folder)
         else:
             print("{} <- Accepted.".format(images))
             img.close()
-            shutil.move(full_image_path, new_folder_name)
-
+            shutil.move(full_image_path, accepted_folder)
+        
+    if os.name == "posix":
+        os.system("sudo chmod 777 -R Accepted/")
+        os.system("sudo chmod 777 -R Rejected/")
+        print("Fixing permissions.")
+        
 if __name__ == "__main__":
     extractor()
